8c073fa1b978ada6b96c8c75bbdd4dc2684d71ed
Leandro Coelho <leandrocs1500@gmail.com>
2024-05-10 22:05:06 -0300

    README

---
+++ PythonPowerUp/README.mk
@@ -0,0 +1,11 @@
+# Jornada Python
+### Repositório para fins educacionais do curso de Python da Jornada Python, onde é abordado, temas como:
+
+* 📝 Python para automação de tarefas; 📝
+* 📊 Python para análise de dados; 📊
+* 🤖 Python para inteligência artificial; 🤖
+* 🖥️ Python para desenvolvimento. 🖥️
+
+### Vale destaquer os arquivos no formato Jupyter Notebook, são encontrado de três formas: **.html**,**.pdf** e **.py**. Para facilitar a leitura dos arquivos, cabe ao leitor escolher qual forma irá vizualizar.
+
+### O curso é oferecido pelo canal Hashtag Programação, no YouTube, que pode ser encontrado no link: [Hashtag Programação](https://www.youtube.com/channel/UCafFexaRoRylOKdzGBU6Pgg)
